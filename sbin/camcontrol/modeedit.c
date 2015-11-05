ColinIanKing/unix-history-repo

commit 11e3567e1d0ac3b0e90938351d34280f0ea33ae8

This is an overhaul of the mode page handling in camcontrol as well as related patches. These include: 	* Mode page editting can be scripted. This involves two 	  things: first, if stdin is not a tty, changes are read from 	  stdin rather than invoking $EDITOR. Second, and more 	  importantly, not all modepage entries must be included in the 	  change set. This means that camcontrol can now gracefully handle 	  more intrusive editting from the $EDITOR, including removal or 	  rearrangement of lines. It also means that you can do stuff 	  like: 		# echo "WCE: 1" | camcontrol modepage da3 -m 8 -e 		# newfs /dev/da3 		# echo "WCE: 0" | camcontrol modepage da3 -m 8 -e 	* Range-checking on user-supplied input values. modeedit.c now 	  uses the field width specifiers to determine the maximum 	  allowable value for a field. If the user enters a value larger 	  than the maximum, it clips the value to the max and warns the 	  user. This also involved patching cam_cmdparse.c to be more 	  consistent with regards to the "count" parameter to arg_put 	  (previously is was the length of strings and 1 for all integral 	  types). The cam_cdbparse(3) man page was also updated to reflect 	  the revised semantics. 	* In the process, I removed the 64 entry limit on mode pages (not 	  that we were even close to hitting that limit). This was a nice 	  side-effect of the other changes. 	* Technically, the new mode editting functionality allows editting 	  of character array entries in mode pages (type 'c' or 'z'), 	  however since buff_encode doesn't grok them it is currently 	  useless. 	* Camcontrol gained two new options related to mode pages: -l and 	  -b. The former lists all available mode pages for a given 	  device. The latter forces mode page display in binary format 	  (the default when no mode page definition was found in 	  scsi_modes). 	* Added support for mode page names to scsi_modes. Allows names to 	  be displayed alongside mode numbers in the mode page 	  listing. Update

share/misc/scsi_modes
@@ -27,13 +27,14 @@
 # LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
-
+#
+# $FreeBSD$
+#
 # The ordering is alphabetical by page name, as it appears in the SCSI spec.
 
 # ALL DEVICE TYPES
 
-# Control mode page:
-0x0a {
+0x0a "Control Mode Page" {
 	{Reserved} *t7
 	{RLEC} t1
 	{Queue Algorithm Modifier} t4
@@ -49,8 +50,7 @@
 	{Ready AEN Holdoff Period} i2
 }
 
-# Disconnect-Reconnect Page:
-0x02	{
+0x02 "Disconnect-Reconnect Page" {
 	{Buffer Full Ratio} i1
 	{Buffer Empty Ratio} i1
 	{Bus Inactivity Limit} i2
@@ -64,8 +64,7 @@
 	{Reserved} *i1
 }
 
-# Peripheral Device Page:
-0x09	{
+0x09 "Peripheral Device Page" {
 	{Interface Identifier} i2
 	{Reserved} *i1
 	{Reserved} *i1
@@ -75,8 +74,7 @@
 
 
 # DIRECT ACCESS DEVICES
-# Caching page:
-0x08 {
+0x08 "Caching Page" {
 	{IC} t1
 	{ABPF} t1
 	{CAP} t1
@@ -93,8 +91,7 @@
 	{Maximum Pre-fetch Ceiling} i2
 }
 
-# Flexible disk page:
-0x05 {
+0x05 "Flexible Disk Page" {
 	{Transfer rate} i2
 	{Number of heads} i1
 	{Sectors per track} i1
@@ -125,8 +122,7 @@
 	{Reserved} *i1
 }
 
-# Format device page:
-0x03 {
+0x03 "Format Device Page" {
 	{Tracks per Zone} i2
 	{Alternate Sectors per Zone} i2
 	{Alternate Tracks per Zone} i2
@@ -143,8 +139,7 @@
 	{Reserved} *t4
 }
 
-# Medium types supported page:
-0x0b	{
+0x0b "Medium Types Supported Page" {
 	{Reserved} *i1
 	{Reserved} *i1
 	{Medium type one supported} i1
@@ -154,9 +149,9 @@
 }
 
 # Notch page (0x0c)
+0x0c "Notch and Partition Page";
 
-#  Read-Write Error Recovery Page
-0x01	{
+0x01 "Read-Write Error Recovery Page" {
 	{AWRE (Auto Write Reallocation Enbld)} t1
 	{ARRE (Auto Read Reallocation Enbld)} t1
 	{TB (Transfer Block)} t1
@@ -175,8 +170,7 @@
 	{Recovery Time Limit} i2
 }
 
-# Rigid Disk Drive Geometry Page
-0x04	{
+0x04 "Rigid Disk Drive Geometry Page" {
 	{Number of Cylinders} i3
 	{Number of Heads} i1
 	{Starting Cylinder-Write Precompensation} i3
@@ -192,8 +186,7 @@
 	{Reserved} *i1
 }
 
-# Verify Error Recovery Page
-0x07	{
+0x07 "Verify Error Recovery Page" {
 	{Reserved} *t4
 	{EER} t1
 	{PER} t1
@@ -209,8 +202,7 @@
 	{Verify Recovery Time Limit} i2
 }
 
-# CD-ROM Audio Control Parameters Page
-0x0E	{
+0x0E "CD-ROM Audio Control Parameters Page" {
 	{Reserved} *t5
 	{Immed} t1
 	{SOTC} t1
@@ -246,7 +238,7 @@
 	{Data Buffer Recovery} t1
 	{Block Identifiers Support} t1
 	{Report Setmarks} t1
-	{AAutomatic Velocity Control} t1
+	{Automatic Velocity Control} t1
 	{Stop on Consecutive Filemarks} t2
 	{Recover Buffer Order} t1
 	{Report Early-Warning} t1
@@ -259,3 +251,5 @@
 	{Select Data Compression Algorithm} i1
 	{Reserved} *i1
 }
+
+0x00 "Vendor-Specific";

lib/libcam/cam_cdbparse.3
@@ -444,9 +444,9 @@ function.
 .It Fa val
 is a void pointer to the value being passed into the function.
 .It Fa count
-is the number of arguments being passed into the
+is the size of the value being passed into the
 .Fn arg_put
-function.  At present this will only be set to 1.
+function.  The argument format determines the unit of measure.
 .It Fa name
 This is a text description of the field, if one was provided in the
 .Fa fmt .

lib/libcam/scsi_cmdparse.c
@@ -119,7 +119,8 @@ do_buff_decode(u_int8_t *databuf, size_t len,
 			if (arg_put) \
 				(*arg_put)(puthook, (letter == 't' ? \
 					'b' : letter), \
-					(void *)((long)(ARG)), 1, field_name); \
+					(void *)((long)(ARG)), width, \
+					field_name); \
 			else \
 				*(va_arg(ap, int *)) = (ARG); \
 			assigned++; \

sbin/camcontrol/Makefile
@@ -2,7 +2,7 @@
 MAINTAINER=ken@FreeBSD.ORG
 
 PROG=   camcontrol
-SRCS=   camcontrol.c modeedit.c
+SRCS=   camcontrol.c modeedit.c util.c
 MAN8=   camcontrol.8
 SDIR=	${.CURDIR}/../../sys
 

sbin/camcontrol/camcontrol.8
@@ -87,9 +87,9 @@
 .Ic modepage
 .Op device id
 .Op generic args
-.Aq Fl m Ar page
+.Aq Fl m Ar page \*(Ba Fl l
 .Op Fl P Ar pgctl
-.Op Fl e
+.Op Fl b | Fl e
 .Op Fl d
 .Nm camcontrol
 .Ic cmd
@@ -317,11 +317,23 @@ command takes several arguments:
 .Bl -tag -width 012345678901
 .It Fl d
 Disable block descriptors for mode sense.
+.It Fl b
+Displays mode page data in binary format.
 .It Fl e
-This flag allows the user to edit values in the mode page.
+This flag allows the user to edit values in the mode page.  The user may
+either edit mode page values with the text editor pointed to by his
+.Ev EDITOR
+environment variable, or supply mode page values via standard input, using
+the same format that
+.Nm camcontrol
+uses to display mode page values.  The editor will be invoked if
+.Nm camcontrol
+detects that standard input is terminal.
+.It Fl l
+Lists all available mode pages.
 .It Fl m Ar mode_page
 This specifies the number of the mode page the user would like to view
-and/or edit.  This argument is mandatory.
+and/or edit.  This argument is mandatory unless -l is specified.
 .It Fl P Ar pgctl
 This allows the user to specify the page control field.  Possible values are:
 .Bl -tag -width xxx -compact

sbin/camcontrol/camcontrol.c
@@ -124,7 +124,7 @@ struct camcontrol_opts option_table[] = {
 	{"defectlist", CAM_ARG_READ_DEFECTS, readdefect_opts},
 	{"devlist", CAM_ARG_DEVTREE, NULL},
 	{"periphlist", CAM_ARG_DEVLIST, NULL},
-	{"modepage", CAM_ARG_MODE_PAGE, "dem:P:"},
+	{"modepage", CAM_ARG_MODE_PAGE, "bdelm:P:"},
 	{"tags", CAM_ARG_TAG, "N:q"},
 	{"negotiate", CAM_ARG_RATE, negotiate_opts},
 	{"rate", CAM_ARG_RATE, negotiate_opts},
@@ -1554,14 +1554,21 @@ modepage(struct cam_device *device, int argc, char **argv, char *combinedopt,
 	 int retry_count, int timeout)
 {
 	int c, mode_page = -1, page_control = 0;
+	int binary = 0, list = 0;
 
 	while ((c = getopt(argc, argv, combinedopt)) != -1) {
 		switch(c) {
+		case 'b':
+			binary = 1;
+			break;
 		case 'd':
 			arglist |= CAM_ARG_DBD;
 			break;
 		case 'e':
 			arglist |= CAM_ARG_MODE_EDIT;
+			break;
+		case 'l':
+			list = 1;
 			break;
 		case 'm':
 			mode_page = strtol(optarg, NULL, 0);
@@ -1580,11 +1587,17 @@ modepage(struct cam_device *device, int argc, char **argv, char *combinedopt,
 		}
 	}
 
-	if (mode_page == -1)
+	if (mode_page == -1 && list == 0)
 		errx(1, "you must specify a mode page!");
 
-	mode_edit(device, mode_page, page_control, arglist & CAM_ARG_DBD,
-		  arglist & CAM_ARG_MODE_EDIT, retry_count, timeout);
+	if (list) {
+		mode_list(device, page_control, arglist & CAM_ARG_DBD,
+		    retry_count, timeout);
+	} else {
+		mode_edit(device, mode_page, page_control,
+		    arglist & CAM_ARG_DBD, arglist & CAM_ARG_MODE_EDIT, binary,
+		    retry_count, timeout);
+	}
 }
 
 static int
@@ -2975,8 +2988,8 @@ usage(int verbose)
 "        camcontrol rescan     <bus[:target:lun]>\n"
 "        camcontrol reset      <bus[:target:lun]>\n"
 "        camcontrol defects    [dev_id][generic args] <-f format> [-P][-G]\n"
-"        camcontrol modepage   [dev_id][generic args] <-m page> [-P pagectl]\n"
-"                              [-e][-d]\n"
+"        camcontrol modepage   [dev_id][generic args] <-m page | -l>\n"
+"                              [-P pagectl][-e | -b][-d]\n"
 "        camcontrol cmd        [dev_id][generic args] <-c cmd [args]>\n"
 "                              [-i len fmt|-o len fmt [args]]\n"
 "        camcontrol debug      [-I][-T][-S][-c] <all|bus[:target[:lun]]|off>\n"
@@ -3022,6 +3035,7 @@ usage(int verbose)
 "modepage arguments:\n"
 "-m page           specify the mode page to view or edit\n"
 "-e                edit the specified mode page\n"
+"-b                force view to binary mode\n"
 "-d                disable block descriptors for mode sense\n"
 "-P pgctl          page control field 0-3\n"
 "defects arguments:\n"

sbin/camcontrol/camcontrol.h
@@ -46,7 +46,9 @@ void mode_sense(struct cam_device *device, int mode_page, int page_control,
 void mode_select(struct cam_device *device, int save_pages, int retry_count,
 		 int timeout, u_int8_t *data, int datalen);
 void mode_edit(struct cam_device *device, int page, int page_control, int dbd,
-	       int edit, int retry_count, int timeout);
+	       int edit, int binary, int retry_count, int timeout);
+void mode_list(struct cam_device *device, int page_control, int dbd,
+	       int retry_count, int timeout);
 char *cget(void *hook, char *name);
 int iget(void *hook, char *name);
 void arg_put(void *hook, int letter, void *arg, int count, char *name);

commit 0ece13a9f10945bfdd85d65b8797411aa324a6ef

MFC r289913,r289916:
r289913:

Use 't' (bits) not 'i' (bytes) for describing MRIE (aka
"Method of Reporting Informational Exceptions") in the SCSI mode database as
the field described in X3T10/94-190 (revision 4; page 2, table 1) [1.] is
4 bits wide, not 4 bytes wide

1. http://ftp.t10.org/ftp/t10/document.94/94-190r4.pdf

Bug 200619
Reported by: Michael Baptist <mbaptist@isilon.com>
Submitted by: Lars Skodje <lskodje@isilon.com>
Sponsored by: EMC / Isilon Storage Division

r289916:

Limit RESOLUTION_MAX to INT_MAX, not UINT_MAX (all spelled out) so the
mode value isn't always clipped to -1 when (resolution * size) == 32, which
would have been the case with values => {4i,32b,32t}.

This seems to have been broken in r64382.

PR: 200619
Reported by: Michael Baptist
Submitted by: Lars Skodje
Sponsored by: EMC / Isilon Storage Division

sbin/camcontrol/modeedit.c
@@ -246,7 +246,7 @@ editentry_set(char *name, char *newvalue, int editonly)
  *     currently workaround it (even for int64's), so we have to kludge it.
  */
 #define	RESOLUTION_MAX(size) ((resolution * (size) == 32)? 		\
-	(int)0xffffffff: (1 << (resolution * (size))) - 1)
+	INT_MAX: (1 << (resolution * (size))) - 1)
 
 	assert(newvalue != NULL);
 	if (*newvalue == '\0')

share/misc/scsi_modes
@@ -106,7 +106,7 @@
 	{EBACKERR} t1
 	{LogErr} t1
 	{Reserved} *t4
-	{MRIE} b4
+	{MRIE} t4
 	{Interval Timer} i4
 	{Report Count} i4
 }